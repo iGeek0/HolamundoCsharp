42c549ef281c2592716219d2796a07d828f5b973
JesusC <jesus.cardenas.d@gmail.com>
2015-11-02 21:38:10 -0600

    Agregando el codigo al directorio

---
+++ HolaGit/HolaGit/Form1.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace HolaGit
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            //BOTON ok
+            MessageBox.Show(Saludar(textBox1.Text));
+            
+        }
+
+
+        private string Saludar(string nombre)
+        {
+            return "Hola como estas " + nombre + " gracias por usar esta aplicacion";
+        }
+    }
+}
